5acf2e95c857acf8944cb91ad0409eee858fc855
Stephen Koch <47271868+kochsj@users.noreply.github.com>
2019-12-27 18:18:29 -0800

    created turn method. handles the flow of the player's turn.

--- game_of_greed_remastered.py
+++ game_of_greed_remastered.py
@@ -30,14 +30,94 @@ class Game:
         self._print('Welcome to Game of Greed')
         response = self._input('Wanna play? ')
         if response == 'y':
-            pass
+            rounds = 0
+            while self.total_score < 10_000 and rounds < 11:
+                self._print(f'Starting Round: {rounds}')
+                self.total_score += self._turn()
+                rounds += 1
+            self._print(f'Rounds: {rounds} Final score: {self.total_score}')
         else:
             self._print('OK. Maybe later')
+
+    def _turn(self):
+        """
+        Method that handles player's turn.
+        Turns continue WHILE there are scoring dice from a dice roll.
+        Player can reroll or choose to end their turn.
+        Returns the turn score.
+        """
+        turn_score = 0
+        dice_aside = ''
+
+# Roll Dice to start the turn ######################################################
+        while True:
+            current_dice_roll = roll_dice(6 - (len(dice_aside) % 6))
+            roll_score = self.calculate_score(current_dice_roll)
+
+# Check for zilch ##################################################################
+            if roll_score == 0:
+                self._print('ZILCH! Sorry you rolled 0 points. Round over.')
+                return 0
+
+# Print the roll ###################################################################
+            self._print(current_dice_roll)
+            self._print(f'Round Score: {turn_score}')
+            self._print(f'Saved Dice: {dice_aside}')
             
-    # Handle calculating score for dice roll
-    def calculate_score(self, current_dice_roll=(2,2,4,4,6,6)):      
-        distribution_of_dice = collections.Counter(current_dice_roll)
+# Ask player to set aside ##########################################################
+            invalid_response = True
+            while invalid_response:
+                response = self._input('What dice will you set aside? ')
+
+# check that the player's response is in the current roll ##########################
+                if response.isnumeric():
+                    mutable_roll = collections.Counter(current_dice_roll)
+                    for char in response:
+                        if int(char) in mutable_roll and mutable_roll[int(char)] > 0:
+                            dice_aside += char
+                            mutable_roll[int(char)] -=1
+                        else:
+                            self._print(f'{response} is an invalid response...')
+                            dice_aside = ''
+                            continue
+                    
+                    turn_score += self.calculate_score(dice_aside)
+                    invalid_response = False
+                else:
+                    self._print(f'{response} is an invalid response...')
+
+# let player know where they stand, ask if they want to roll again #################
+            if len(dice_aside) == 6:
+                self._print(f'SWEEP!! You scored with all six dice! Rolling six again!')
+                dice_aside = ''
+            else:    
+                self._print(f'You entered: {dice_aside}')
+                self._print(f'You saved: {turn_score} points')
+                response = self._input(f'Roll again? y/n')
+            if response == 'n':
+                return turn_score
+
+                
+                
+
+    def calculate_score(self, current_dice_roll=(2,2,4,4,6,6)):
+        """
+        Accepts a dice roll as a parameter.
+        Returns the calculated score.
+        """
+
+        # Cleaning input data for use
+        if isinstance(current_dice_roll, str):
+            formatted_dice_roll = (int(num) for num in current_dice_roll)    
+            distribution_of_dice = collections.Counter(formatted_dice_roll)
+        elif isinstance(current_dice_roll, tuple):
+            distribution_of_dice = collections.Counter(current_dice_roll)
+        else:
+            return "Invalid dice roll."
+
+        # Roll Calculation
         roll_score = 0
+
         for num in (1,2,3,4,5,6):
             a = '{}{}'.format(num, distribution_of_dice[num])
             if len(current_dice_roll) == 6 and set(distribution_of_dice.values()) == {2}: #three pairs
@@ -61,5 +141,4 @@ class Game:
                     roll_score += 50*int(a[1])
             elif a[1] >= '3': # 3 or more of a kind
                 roll_score += 100*int(a[0])*(int(a[1])-2)
-        # The output from calculate_score is an integer representing the roll’s score according to rules of game.
         return roll_score
